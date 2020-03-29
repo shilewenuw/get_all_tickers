531e3cc8a4b6463229057016672d1c0568b24097
Shile Wen <shilewen@cs.washington.edu>
2020-03-28 18:06:42 -0700

    implemented get_all_tickers.py and added init file

---
+++ get_all_tickers.py
@@ -0,0 +1,37 @@
+import pandas
+
+NYSE_URL = 'https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download'
+NASDAQ_URL = 'https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download'
+AMEX_URL = 'https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download'
+
+
+# get all tickers as a CSV
+def get_tickers(NYSE=True, NASDAQ=True, AMEX=True):
+    tickers_list = []
+    if NYSE:
+        tickers_list.extend(__url2list(NYSE_URL))
+    if NASDAQ:
+        tickers_list.extend(__url2list(NASDAQ_URL))
+    if AMEX:
+        tickers_list.extend(__url2list(AMEX_URL))
+    return tickers_list
+
+
+def __url2list(url):
+    df = pandas.read_csv(url)
+    df_filtered = df[~df['Symbol'].str.contains("\.|\^")]
+    return df_filtered['Symbol'].tolist()
+
+
+# save the tickers to a CSV
+def save_tickers(NYSE=True, NASDAQ=True, AMEX=True, filename='tickers.csv'):
+    tickers = get_tickers(NYSE, NASDAQ, AMEX)
+    df = pandas.DataFrame(tickers)
+    df.to_csv(filename, index=False)
+
+
+if __name__ == '__main__':
+    tickers = get_tickers()
+    print(tickers[:5])
+
+    save_tickers()
