bb23eb0bb82e3ef9c00baf74a17207f8a180e68e
Rik Brown <rbrown@snapchat.com>
2021-05-01 20:35:32 -0700

    remove commented code

--- get_all_tickers/get_tickers.py
+++ get_all_tickers/get_tickers.py
@@ -12,20 +12,6 @@ _SECTORS_LIST = set(['Consumer Non-Durables', 'Capital Goods', 'Health Care',
 
 
 # headers and params used to bypass NASDAQ's anti-scraping mechanism in function __exchange2df
-# headers = {
-#     'authority': 'old.nasdaq.com',
-#     'upgrade-insecure-requests': '1',
-#     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'sec-fetch-site': 'cross-site',
-#     'sec-fetch-mode': 'navigate',
-#     'sec-fetch-user': '?1',
-#     'sec-fetch-dest': 'document',
-#     'referer': 'https://github.com/shilewenuw/get_all_tickers/issues/2',
-#     'accept-language': 'en-US,en;q=0.9',
-#     'cookie': 'AKA_A2=A; NSC_W.TJUFEFGFOEFS.OBTEBR.443=ffffffffc3a0f70e45525d5f4f58455e445a4a42378b',
-# }
-
 headers = {
     'authority': 'api.nasdaq.com',
     'accept': 'application/json, text/plain, */*',
